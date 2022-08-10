466d952927b4cbf0e1de2f202a428c697101f716
Owusu-Desmond <desmond0406114009@gmail.com>
2022-08-10 11:40:02 +0000

    Create page footer

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+const Footer = () => (
+  <footer className="footer">
+    <p>
+      Created by
+      <a href="https://github.com/Owusu-Desmond">
+        {' '}
+        <i> Desmond Owusu Ansah</i>
+      </a>
+      . The source code is licensed
+      <a href="https://github.com/Owusu-Desmond/todos/blob/dev/LICENSE"> MIT</a>
+    </p>
+    <p> &copy; 2022 - Todo List App || All rights all reserved</p>
+  </footer>
+);
+
+export default Footer;
